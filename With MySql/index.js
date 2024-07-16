fd6a76685013e1a611116d01647e40d45321c9ba
admin <admin@DESKTOP-PHVNJ4P>
2024-07-16 20:42:31 +0530

    adding post method

--- With MySql/index.js
+++ With MySql/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import db from "./config.js";
+import bodyParser from "body-parser";
 
 
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json())
+
+// Parsing the JSON body : refers to the process of converting a JSON-formatted string sent in an HTTP request 
+// into a JavaScript object that can be easily accessed and manipulated within your application.
+
+// body-parser : is a middleware used to parse incoming request bodies before your handlers,
+// making the data available in the req.body property.
+
 
 
 
@@ -15,7 +26,7 @@ console.warn("Result: ",result);
 })
 
 
-
+//// Get 
 app.get("/",(req,res)=>{
 const sql = `select * from employee`;
 db.query(sql,(err,result)=>{
@@ -28,6 +39,45 @@ db.query(sql,(err,result)=>{
 })
 
 })
+
+
+//// Post
+
+app.post("/inserting_data",(req,res)=>{
+    const data = req.body;    // Access the parsed JSON data
+    const sql = "insert into employee set ?"
+    db.query(sql,data,(err,result)=>{
+        if(err){
+            res.status(500).send("Unexpected Error");
+            return;
+        }else{
+            res.status(200).send({result, message:"Data inserted successfully"});
+        }
+    })
+})
+//SET: This keyword is used to specify the columns and their corresponding values for the insertion.
+// ?: This is a placeholder that will be replaced by the data object passed as an argument in the query method.
+// This approach helps to prevent SQL injection attacks by escaping the values properly.
+//   req.body : extracting the JSON data from the request body sent by the client.
+
+
+
+app.post("/inserting_data_1",(req,res)=>{
+    const {e_name,e_phone,e_address} = req.body;
+    const sql = `insert into employee (e_name,e_phone,e_address) values(?,?,?)`;
+
+    db.query(sql,[e_name,e_phone,e_address],(err,result)=>{
+        if(err){
+            res.status(500).send("Unexpected Error");
+            return;
+        }else{
+            res.status(200).send({result, message:"Data inserted successfully"});
+        }
+    })
+})
+
+
+
 
 
 
